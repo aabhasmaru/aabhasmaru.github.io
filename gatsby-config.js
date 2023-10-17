199e958645d4c9c656a9f0866d521b32de3c5342
Aashi Shrimal <shrimalaashi07@gmail.com>
2023-10-16 22:04:22 -0400

    updated static folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Aabhas Maru is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://aabhasmaru.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/am.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
   plugins: [
