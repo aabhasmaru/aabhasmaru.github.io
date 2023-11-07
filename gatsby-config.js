f240fa3077d91cadac9b20a85d5d689a46b20f1e
Aashi Shrimal <shrimalaashi07@gmail.com>
2023-11-06 22:15:29 -0500

    display image updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Aabhas Maru is a Business Analysis enthusiast who specializes in Project Management with exceptional Finance & Accounting knowledge.',
     siteUrl: 'https://aabhasmaru.com', // No trailing slash allowed!
-    image: '/am.png', // Path to your image you placed in the 'static' folder
+    image: '/front.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
   plugins: [
